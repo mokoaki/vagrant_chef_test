e36b139249f1c03e911f5df498db5ec4ed5e3967
mokoaki <mokoriso@gmail.com>
2015-09-16 02:36:05 +0900

    SELINUX無効化 不要っぽいサービス無効化

--- cookbooks/rails_system/recipes/init.rb
+++ cookbooks/rails_system/recipes/init.rb
@@ -14,16 +14,42 @@ end
   end
 end
 
-service 'firewalld' do
-  action [:disable, :stop]
+%w{firewalld kdump postfix}.each do |svc|
+  service svc do
+    action [:disable, :stop]
+  end
+end
+
+# setenforce 0で一時的にSELinuxを無効化し、
+# /etc/selinux/configの作成を通知する
+# selinuxenabledコマンドの終了ステータスが0(selinuxが有効)の場合だけ実行される
+execute "disable selinux enforcement" do
+  only_if "which selinuxenabled && selinuxenabled"
+  command "setenforce 0"
+  action :run
+
+  # 呼び出す
+  notifies :create, "template[/etc/selinux/config]"
+end
+
+# 再起動の際もSELinuxの無効状態を維持するために、
+# /etc/selinux/configに、設定を記述する
+template "/etc/selinux/config" do
+  source "selinux_config.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+
+  # 通常は動作しない
+  action :nothing
 end
 
 template "/etc/ntp.conf" do
+  source "ntp.conf.erb"
   path "/etc/ntp.conf"
   owner "root"
   group "root"
   mode "0644"
-  source "ntp.conf.erb"
 end
 
 service 'ntpd' do
