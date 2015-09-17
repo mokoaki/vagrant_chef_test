acbcc5e3f7523c12a0874a8c5059ba536224ccab
mokoaki <mokoriso@gmail.com>
2015-09-18 00:20:39 +0900

    Rbenvのインスコ修正 しつこいくらいuser,groupをvagrantに

--- cookbooks/rails_system/recipes/rbenv.rb
+++ cookbooks/rails_system/recipes/rbenv.rb
@@ -6,68 +6,57 @@ target_version = node['rbenv']['version']
   end
 end
 
-#本当に要るのか
-group "rbenv" do
-  action :create
-  members "vagrant"
-  append true
+git "/home/vagrant/.rbenv" do
+  repository "https://github.com/sstephenson/rbenv.git"
+  revision   "master"
+  user       "vagrant"
+  group      "vagrant"
+  action     :sync
 end
 
-git '/usr/local/rbenv' do
-  repository 'https://github.com/sstephenson/rbenv.git'
-  # reference 'master'
-  user 'root'
-  group 'root'
-  action :sync
-end
-
-directory "/usr/local/rbenv/shims" do
-  owner 'root'
-  group 'root'
-  mode 00755
+directory "/home/vagrant/.rbenv/plugins" do
+  owner  "vagrant"
+  group  "vagrant"
   action :create
 end
 
-directory "/usr/local/rbenv/versions" do
-  owner 'root'
-  group 'root'
-  mode 00755
-  action :create
-end
-
-directory "/usr/local/rbenv/plugins" do
-  owner 'root'
-  group 'root'
-  mode 00755
-  action :create
-end
-
-git '/usr/local/rbenv/plugins/ruby-build' do
-  repository 'https://github.com/sstephenson/ruby-build.git'
-  # reference 'master'
-  user 'root'
-  group 'root'
-  action :sync
+git "/home/vagrant/.rbenv/plugins/ruby-build" do
+  repository "https://github.com/sstephenson/ruby-build.git"
+  revision   "master"
+  user       "vagrant"
+  group      "vagrant"
+  action     :sync
 end
 
 template "/etc/profile.d/rbenv.sh" do
   path "/etc/profile.d/rbenv.sh"
-  owner "root"
-  group "root"
+  owner  "vagrant"
+  group  "vagrant"
   mode "0644"
   source "rbenv.sh.erb"
 end
 
 execute "rbenv install #{target_version}" do
-  # user   "vagrant"
-  # cwd    "/home/vagrant"
+  user   "vagrant"
+  group  "vagrant"
+  cwd    "/home/vagrant"
   command   "source /etc/profile.d/rbenv.sh; CONFIGURE_OPTS=--disable-install-rdoc rbenv install #{target_version} -v"
   action :run
-  not_if { ::File.exists? "/usr/local/rbenv/versions/#{target_version}" }
+  not_if { ::File.exists? "/home/vagrant/.rbenv/versions/#{target_version}" }
 end
 
 execute "rbnev global #{target_version}" do
-  # not_if "source /etc/profile.d/rbenv.sh; rbenv global #{target_version}"
+  user   "vagrant"
+  group  "vagrant"
+  cwd    "/home/vagrant"
   command "source /etc/profile.d/rbenv.sh; rbenv global #{target_version}; rbenv rehash"
+  action :run
+end
+
+bash "rbenv global 2.2.3" do
+  user   "vagrant"
+  group  "vagrant"
+  cwd    "/home/vagrant"
+  code   "source /etc/profile.d/rbenv.sh; rbenv global 2.2.3; rbenv rehash"
   action :run
 end
