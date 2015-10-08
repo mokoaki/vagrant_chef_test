40df8cd72a8b59ec06aadc2cfdca61a760d16085
mokoaki <mokoriso@gmail.com>
2015-10-09 00:30:11 +0900

    READMEに再起動の件を追記、あとRedisのキャッシュについての説明を追記 ていうか、再起動しなくてもいいようにしたいね〜

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ActionController::Base
 
     @redis_test_data = Rails.cache.fetch('temp_key', expires_in: 10.seconds) do
       sleep 3
-      'この文字列はredisに入っていたデータです キャッシュを使用しなければ3秒待つようになっています。次回はキャッシュから高速に返答するはずです。このredisのキャッシュは10秒間だけ有効です'
+      'この文字列はredisに入っていたデータです。キャッシュが無ければ3秒待ってキャッシュを作るようになっています。このredisのキャッシュは10秒間だけ有効です。つまり10秒以内なら、次回はキャッシュから高速に返答するはずです。'
     end
   end
 end
