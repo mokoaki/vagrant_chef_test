35e8c77e8c57b1cf72fec6686580e877b71096aa
mokoaki <mokoriso@gmail.com>
2015-10-09 22:36:01 +0900

    デフォでインスコするgemのupdateにonly_ifを追加

--- cookbooks/rails_system/recipes/rails.rb
+++ cookbooks/rails_system/recipes/rails.rb
@@ -5,6 +5,19 @@
 end
 
 node['rbenv']['ruby']['default_gems'].each do |gem|
+  bash "gem update #{gem['name']}" do
+    user   'vagrant'
+    group  'vagrant'
+    cwd    '/home/vagrant'
+
+    code <<-EOH
+      source /etc/profile.d/rbenv.sh
+      gem update #{gem['name']} #{gem['options']}
+    EOH
+
+    only_if "source /etc/profile.d/rbenv.sh; gem list | grep #{gem['name']}"
+  end
+
   bash "gem install #{gem['name']}" do
     user   'vagrant'
     group  'vagrant'
@@ -16,17 +29,6 @@ node['rbenv']['ruby']['default_gems'].each do |gem|
     EOH
 
     not_if "source /etc/profile.d/rbenv.sh; gem list | grep #{gem['name']}"
-  end
-
-  bash "gem update #{gem['name']}" do
-    user   'vagrant'
-    group  'vagrant'
-    cwd    '/home/vagrant'
-
-    code <<-EOH
-      source /etc/profile.d/rbenv.sh
-      gem update #{gem['name']} #{gem['options']}
-    EOH
   end
 end
 
