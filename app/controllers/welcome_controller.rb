3f5dc1337dd29aeef9f66784b56b3a092d80fb0a
mokoaki <mokoriso@gmail.com>
2016-01-25 12:26:14 +0900

    画面表示をちょっち修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ActionController::Base
   def index
     @user = User.first
 
-    session[:session_data] ||= "この文字列はセッションデータです。このキャッシュは1日間だけ有効です。このセッションは #{Time.now} 頃に作られました"
+    session[:session_data] ||= "この文字列はセッションデータです。Redis内に保存されています。このセッションは1日間だけ有効です。このセッションは #{Time.now} 頃に作られました"
 
     @cache_data = Rails.cache.fetch('temp_cache_key', expires_in: 10.seconds) do
       "この文字列はredisでキャッシュしたデータです。このキャッシュは10秒間だけ有効です。このキャッシュは #{Time.now} 頃に作られました"
